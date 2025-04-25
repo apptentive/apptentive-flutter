4d0409efeed5e3128ce0757ec306d4cdb5d28d36
Frank Schmitt <frank.schmitt@alchemer.com>
2025-04-25 10:45:34 -0700

    Upgrade flutter packages and extract credentials to new file

---
+++ example/lib/credentials-template.dart
@@ -0,0 +1,6 @@
+class ApptentiveCredentials {
+  static const String ApptentiveKeyIOS = "<YOUR_IOS_KEY>";
+  static const String ApptentiveSignatureIOS = "<YOUR_IOS_SIGNATURE>";
+  static const String ApptentiveKeyAndroid = "<YOUR_ANDROID_KEY>";
+  static const String ApptentiveSignatureAndroid = "<YOUR_ANDROID_SIGNATURE>";
+}
