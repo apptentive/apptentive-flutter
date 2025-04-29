4cefd1e5d6cfb87b3c32e243b70d3aeb02a48e9a
Frank Schmitt <frank.schmitt@alchemer.com>
2025-04-29 11:18:39 -0700

    Use correct member names in credentials-template.dart

--- example/lib/credentials-template.dart
+++ example/lib/credentials-template.dart
@@ -1,6 +1,6 @@
 class ApptentiveCredentials {
-  static const String ApptentiveKeyIOS = "<YOUR_IOS_KEY>";
-  static const String ApptentiveSignatureIOS = "<YOUR_IOS_SIGNATURE>";
-  static const String ApptentiveKeyAndroid = "<YOUR_ANDROID_KEY>";
-  static const String ApptentiveSignatureAndroid = "<YOUR_ANDROID_SIGNATURE>";
+  static const String iOSKey = "<YOUR_IOS_KEY>";
+  static const String iOSSignature = "<YOUR_IOS_SIGNATURE>";
+  static const String androidKey = "<YOUR_ANDROID_KEY>";
+  static const String androidSignature = "<YOUR_ANDROID_SIGNATURE>";
 }
